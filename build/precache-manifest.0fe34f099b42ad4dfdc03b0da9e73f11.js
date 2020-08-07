064bb8df18af3d090c8e5709a83c6c4abfb5205b
arakhimov <artemiirakhimov@yandex.ru>
2020-08-07 21:10:54 +0300

    project build

---
+++ build/precache-manifest.0fe34f099b42ad4dfdc03b0da9e73f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4070a1985304e65f2b42430d2e13880c",
+    "url": "/planktonics-intranet/auth/index.html"
+  },
+  {
+    "revision": "dd1c8b12c08b2317639b",
+    "url": "/planktonics-intranet/auth/static/css/main.1c906e3b.chunk.css"
+  },
+  {
+    "revision": "2e5c9e21c9892392a6ab",
+    "url": "/planktonics-intranet/auth/static/js/2.3f390e4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/planktonics-intranet/auth/static/js/2.3f390e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1c8b12c08b2317639b",
+    "url": "/planktonics-intranet/auth/static/js/main.58611769.chunk.js"
+  },
+  {
+    "revision": "7cec1d43eca255f3b14c",
+    "url": "/planktonics-intranet/auth/static/js/runtime-main.6b77d039.js"
+  },
+  {
+    "revision": "a50f9c96a76356e3d01013e0b042989f",
+    "url": "/planktonics-intranet/auth/static/media/Comic_Sans_MS.a50f9c96.ttf"
+  }
+]);
